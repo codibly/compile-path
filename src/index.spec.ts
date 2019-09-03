90c5367fd70dd6267f53a470ee2f54c2d7643f0f
Damian Łabas <damian.labas@codibly.com>
2019-09-03 15:49:10 +0200

    feat: compile path with basic tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,38 @@
-describe("Test", () => {
-  it("should make trivial assertion", () => {
-    expect(true).toEqual(true);
+import { compilePath } from "./index";
+
+const BASE_ROUTE = "/base-route";
+
+describe("compile-path", () => {
+  it("should create simple route", () => {
+    expect(compilePath(BASE_ROUTE)).toEqual("/base-route");
+  });
+
+  it("should create route with params", () => {
+    const params = { id: 10 };
+
+    expect(compilePath(`${BASE_ROUTE}/:id`, params)).toEqual("/base-route/10");
+  });
+
+  it("should create route with params & query", () => {
+    const params = { id: 10 };
+    const query = { name: "Codibly" };
+
+    expect(compilePath(`${BASE_ROUTE}/:id`, params, query)).toEqual(
+      "/base-route/10?name=Codibly"
+    );
+  });
+
+  it("should create route with undefined params and query", () => {
+    expect(compilePath(BASE_ROUTE, undefined, undefined)).toEqual(
+      "/base-route"
+    );
+  });
+
+  it("should create route with undefined params and simple query", () => {
+    const query = { name: "Codibly" };
+
+    expect(compilePath(BASE_ROUTE, undefined, query)).toEqual(
+      "/base-route?name=Codibly"
+    );
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,31 @@
-// tslint:disable-next-line
-console.log("Hello world");
+import { compile, PathFunction } from "path-to-regexp";
+import { stringify as stringifyQuery } from "qs";
+
+interface CompilePathParams {
+  [key: string]: string | number | string[] | number[];
+}
+interface CompilePathQuery {
+  [key: string]: any;
+}
+interface CompilePathCache {
+  [key: string]: PathFunction;
+}
+
+const compilePathCache: CompilePathCache = {};
+
+export function compilePath(
+  pattern: string,
+  params?: CompilePathParams,
+  query?: CompilePathQuery
+): string {
+  if (!compilePathCache[pattern]) {
+    compilePathCache[pattern] = compile(pattern);
+  }
+
+  const compiledPath = compilePathCache[pattern](params);
+  const stringifiedQuery = query ? stringifyQuery(query) : null;
+
+  return stringifiedQuery
+    ? `${compiledPath}?${stringifiedQuery}`.trim()
+    : compiledPath.trim();
+}
